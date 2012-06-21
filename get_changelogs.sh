653b1b70c48e01da804ab6002020936eb9585461
Ugo Cupcic <ugo@ugo-work>
2012-06-21 09:46:14 +0200

    getting the stack names from a rosinstall file

--- get_changelogs.sh
+++ get_changelogs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-STACKS=(shadow_robot shadow_robot_ethercat sr_contrib sr_demo sr_visualization sr_teleop denso object_manipulation sr_object_manipulation sr_config)
+#first we delete the existing changelog:
+rm changelogs/*
+
+TMP_STACKS=`awk < $1 ' $1=="local-name:" { print $2 }'`
+#split in an array
+STACKS=($(echo ${TMP_STACKS} | tr " " "\n"))
 
 for (( i = 0 ; i < ${#STACKS[@]} ; i++ )); do
-  echo ${STACKS[$i]}
+  echo "getting changelog[$i]: "${STACKS[$i]}
   if [ -f `rosstack find ${STACKS[$i]}`/changelog.tex ]; then
     cp `rosstack find ${STACKS[$i]}`/changelog.tex changelogs/file${i}.tex
   fi
